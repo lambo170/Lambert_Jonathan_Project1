2373ae8fc22bdf1b7b2cc94724f22a70610957a1
Jonathan Lambert <lambo170@gmail.com>
2012-01-12 14:05:55 -0800

    Store Data II

--- main.js
+++ main.js
@@ -22,6 +22,45 @@ window.addEventListener("DOMContentLoaded", function(){
 		}
 		selectLi.appendChild(makeSelect);
 	}
+	
+	//Paperwork Radio
+	function getSelectedRadio(){
+		var radios = document.forms[0].paper;
+		for(var i=0; i<radios.length; i++){
+			if(radios[i].checked){
+				paperValue = radios[i].value;
+			}
+		}
+	}
+	
+	function getCheckboxValue(){
+		if($('flak').checked){
+			flakValue = $('flak')
+		}else{
+			flakValue = "No"
+		}
+			if($('helmet').checked){
+				helmetValue = $('helmet')
+			}else{
+				helmetValue = "No"
+			}
+				if($('frog').checked){
+					frogValue = $('frog')
+				}else{
+					frogValue = "No"
+				}
+				 	if($('cammies').checked){
+						cammiesValue = $('cammies')
+					}else{
+						cammiesValue = "No"
+					}
+						if($('misc').checked){
+							miscValue = $('misc')
+						}else{
+							miscValue = "No"
+						}
+	}
+	
 	function storeData(){
 		var id 	= Math.floor(Math.random()*10000001);
 		var item			 = {};
@@ -32,18 +71,27 @@ window.addEventListener("DOMContentLoaded", function(){
 			item.age		 = ["Age: ", $('age').value];
 			item.phone		 = ["Phone #: ", $('phone').value];
 			// GEAR
-			item.flakjacket  = ["Flak Jacket: ", $('flak').value];
+			item.flakjacket  = ["Flak Jacket: ", flakValue];
 			item.serial		 = ["Flak Serials: ", $('plate#').value];
-			item.kevlar		 = ["Kevlar: ", $('helmet').value];
-			item.frogGear	 = ["Frog Gear: ", $('frog').value];
-			item.cammies	 = ["Cammies: ", $('cammies').value];
-			item.miscGear	 = ["Misc Gear: ", $('misc').value];
+			item.kevlar		 = ["Kevlar: ", helmetValue];
+			item.frogGear	 = ["Frog Gear: ", frogValue];
+			item.cammies	 = ["Cammies: ", cammiesValue];
+			item.miscGear	 = ["Misc Gear: ", miscValue];
 			// MISC
-			item.medical	 =
-			item.papers		 =
+			item.medical	 = ["Medical Status: ", $('groups').value];
+			item.papers		 = ["Paperwork Status: ", ]
+			
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Data is Saved!");
 	}
 	
-	var medicalCheck = ["Choose:", "Yes", "No", "Almost"];
+	var medicalCheck = ["Choose:", "Yes", "No", "Almost"],
+		paperValue = "No",
+		flakValue = "No",
+		helmetValue = "No",
+		frogValue = "No",
+		cammiesValue = "No",
+		miscValue = "No";
 	makeCats();
 	
 	/*
